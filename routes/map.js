26ee8c717aaa7369fa6b8d76c3246478016957d5
nnewnew <godsnew2542@gmail.com>
2020-07-13 20:26:19 +0700

    Error

---
+++ routes/map.js
@@ -0,0 +1,17 @@
+eqfeed_callback({
+  type: "FeatureCollection",
+  features: [
+    {
+      geometry: {
+        type: "Point",
+        coordinates: [13.66194, 100.6126],
+      },
+    },
+    {
+      geometry: {
+        type: "Point",
+        coordinates: [13.62619, 100.7548],
+      },
+    },
+  ],
+});
--- routes/outlets.js
+++ routes/outlets.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const axios = require("axios");
-const { log } = require("debug");
+// const { log } = require("debug");
 
 /* GET users listing. */
 router.get("/", async function (req, res, next) {
@@ -10,9 +10,21 @@ router.get("/", async function (req, res, next) {
   const GoogleSheetData =
     "https://sheets.googleapis.com/v4/spreadsheets/15qVChEyQ7Gu4rNgZK39gZ_RX4grMWM2EW3F-yk83C_M/values/Outlets!A2:F20?key=AIzaSyB9E4_jgrs0LLdVSGWhif84Tw5l1m18gYo";
   const response = await axios.get(GoogleSheetData);
-  console.log(response.data);
 
-  res.render("outlets", { title: "Outlets", posts: response.data });
+  //   const MapSheetData =
+  //     "https://sheets.googleapis.com/v4/spreadsheets/15qVChEyQ7Gu4rNgZK39gZ_RX4grMWM2EW3F-yk83C_M/values/Outlets!D2:D20?key=AIzaSyB9E4_jgrs0LLdVSGWhif84Tw5l1m18gYo";
+  //   const responseMap = await axios.get(MapSheetData);
+  //   console.log(response.data);
+
+  res.render("outlets", {
+    title: "Outlets",
+    posts: response.data,
+    key: "AIzaSyCdQCEwdWm7PB6InxPfQ-Xlf6P8Vjer2xk",
+  });
+});
+
+router.get("/test", function (req, res, next) {
+  res.render("test", {});
 });
 
 module.exports = router;
