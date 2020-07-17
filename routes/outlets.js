4b17eda5a033029fa1ea882d690077f176ef0ffb
nnewnew <godsnew2542@gmail.com>
2020-07-17 21:23:54 +0700

    page success

--- routes/outlets.js
+++ routes/outlets.js
@@ -4,21 +4,17 @@ const axios = require("axios");
 // const { log } = require("debug");
 
 /* GET users listing. */
-router.get("/", async function (req, res, next) {
-  // axios
-  const GoogleSheetData =
-    "https://sheets.googleapis.com/v4/spreadsheets/15qVChEyQ7Gu4rNgZK39gZ_RX4grMWM2EW3F-yk83C_M/values/Outlets!A2:S20?key=AIzaSyB9E4_jgrs0LLdVSGWhif84Tw5l1m18gYo";
-  const response = await axios.get(GoogleSheetData);
+router.get("/", async function(req, res, next) {
+    // axios
+    const GoogleSheetData =
+        "https://sheets.googleapis.com/v4/spreadsheets/15qVChEyQ7Gu4rNgZK39gZ_RX4grMWM2EW3F-yk83C_M/values/Outlets!A2:S20?key=AIzaSyB9E4_jgrs0LLdVSGWhif84Tw5l1m18gYo";
+    const response = await axios.get(GoogleSheetData);
 
-  res.render("outlets", {
-    title: "Outlets",
-    posts: response.data,
-    // key: "AIzaSyCdQCEwdWm7PB6InxPfQ-Xlf6P8Vjer2xk",
-  });
-});
-
-router.get("/test", function (req, res, next) {
-  res.render("test", {});
+    res.render("outlets", {
+        title: "Branches",
+        posts: response.data,
+        // key: "AIzaSyCdQCEwdWm7PB6InxPfQ-Xlf6P8Vjer2xk",
+    });
 });
 
 module.exports = router;
