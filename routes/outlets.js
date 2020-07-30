9ee403c350aab073c980d43b6b70c0b02aa1f0d2
nnewnew <godsnew2542@gmail.com>
2020-07-31 00:23:56 +0700

    ....

--- routes/outlets.js
+++ routes/outlets.js
@@ -1,12 +1,12 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const axios = require("axios");
 
 router.get("/", async function(req, res, next) {
     // axios
-    const GoogleSheetData =
+    const googleSheetData =
         "https://sheets.googleapis.com/v4/spreadsheets/15qVChEyQ7Gu4rNgZK39gZ_RX4grMWM2EW3F-yk83C_M/values/Outlets!A2:S20?key=AIzaSyB9E4_jgrs0LLdVSGWhif84Tw5l1m18gYo";
-    const response = await axios.get(GoogleSheetData);
+    const response = await axios.get(googleSheetData);
 
     res.render("outlets", {
         title: "Branches",
