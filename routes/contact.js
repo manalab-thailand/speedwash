0fc3edd10b63d8df51b8d03c7a9d4095199db576
DOUBLEPOR <doublepor.pp$gmail.com>
2020-08-03 01:16:16 +0700

    take script map in to file js

--- routes/contact.js
+++ routes/contact.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
-const constants = require("../public/javascripts/constant");
 
 router.get("/", async function (req, res, next) {
-  
-  const googlesheetData = `https://sheets.googleapis.com/v4/spreadsheets/15qVChEyQ7Gu4rNgZK39gZ_RX4grMWM2EW3F-yk83C_M/values/Outlets!A2:S20?key=${constants.GOOGLEKEY}`;
-  const response = await axios.get(googlesheetData);
 
-  res.render("page/contact", { title: "Outlets", dataMap: response.data });
+  res.render("page/contact", { title: "Outlets"});
 });
 
 module.exports = router;
